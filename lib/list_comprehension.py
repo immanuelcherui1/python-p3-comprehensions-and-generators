f7ac1fed08d1d5c4575cd174e286b469449ce2db
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-03-06 19:31:58 +0300

    Done with assignments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(num_list=[]):
+    evens = [x for x in num_list if x % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list == None:
+        return []
+    else:
+        return [sentence + '!' for sentence in sentence_list]
+
